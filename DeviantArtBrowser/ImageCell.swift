9473f7ce9ecf93c4918edc0cb2292de6fe25f862
Darrell Nicholas <darrellwnicholas@gmail.com>
2015-02-05 22:10:50 -0600

    Created ImageCell as a subclass of BasicCell and added an ImageView to it and changed its height. Also reconfigured constraints to make it display properly. Added push segue to the `Deviant Media` scene

---
+++ DeviantArtBrowser/ImageCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageCell.swift
+//  DeviantArtBrowser
+//
+//  Created by Darrell Nicholas on 2/5/15.
+//  Copyright (c) 2015 Razeware, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: BasicCell {
+    
+    @IBOutlet var customImageView: UIImageView!
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
